dcc1d3aa4e399eb55370499965182c165b521c11
bilalbentoumi <bilalbentoumi@gmail.com>
2021-08-20 19:21:39 +0100

    Fix deployment

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const port = process.env.PORT || 5000
+
+app.use('/', express.static(path.join(__dirname, 'www')))
 
 app.get('/api', (req, res) => {
     res.send('Hello from api!')

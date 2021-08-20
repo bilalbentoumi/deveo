3f646686c01f5ef44def38c273d8ee013b3df44e
bilalbentoumi <bilalbentoumi@gmail.com>
2021-08-20 20:40:38 +0100

    Init login screen

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <h2>Home Page</h2>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home'
 }
 </script>
